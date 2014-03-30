6d0d3ab4865e096a686eb884af36134457f228b0
Dominik Tränklein <dominik@bleech.de>
2014-03-30 12:16:10 +0200

    modified modules & includes

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,9 +8,6 @@ Exec {
   user        => $boxen_user,
 
   path => [
-    "${boxen::config::home}/rbenv/shims",
-    "${boxen::config::home}/rbenv/bin",
-    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
     "${boxen::config::home}/homebrew/bin",
     '/usr/bin',
     '/bin',
@@ -56,31 +53,24 @@ node default {
   include dnsmasq
   include git
   include hub
-  include nginx
+  include chrome
+  include firefox
+  include alfred
+  include googledrive
+  include dropbox
+  include libreoffice
+  include wunderlist
+  include onepassword
+  include caffeine
+  include slack
+  include mou
+  include skype
+  include spotify
+  include teamviewer
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
-  }
-
-  # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
-
-  # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-
-  # common, useful packages
-  package {
-    [
-      'ack',
-      'findutils',
-      'gnu-tar'
-    ]:
   }
 
   file { "${boxen::config::srcdir}/our-boxen":
